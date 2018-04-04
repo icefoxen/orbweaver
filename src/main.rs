e70409285633eb0e3bfe68db5db00e9b38a4f366
Simon Heath <icefoxen@gmail.com>
2018-04-04 13:06:57 -0400

    Test parsing with webidl crate
    seems to work.  Need an authorative webidl collection I can use.
    Also a test suite.  :-/

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+extern crate webidl;
+
+use webidl::visitor::ImmutableVisitor;
+
+fn pretty_print(ast: &webidl::ast::AST) {
+    let mut visitor = webidl::visitor::PrettyPrintVisitor::new();
+    visitor.visit(ast);
+    println!("{}", visitor.get_output());
+}
+
 fn main() {
-    println!("Hello, world!");
+    let testcode = r#"/* Example taken from emscripten site */
+                        enum EnumClass_EnumWithinClass {
+                            "EnumClass::e_val"
+                        };"#;
+    let parser = webidl::Parser::new();
+    let res = parser.parse_string(testcode).unwrap();
+    pretty_print(&res);
 }
